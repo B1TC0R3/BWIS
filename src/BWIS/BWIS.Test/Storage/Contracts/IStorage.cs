b791a743ad6bd265ba06482298acb4edcfe4d6bf
Thomas Gingele <thomas@gingele.de>
2021-12-20 17:11:56 +0100

    Add Tests for IStorage, IStorageItem

---
+++ src/BWIS/BWIS.Test/Storage/Contracts/IStorage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BWIS.Test.Storage.Contracts
+{
+    public class IStorage
+    {
+        [Fact]
+        public static void CanBeReferenced() 
+        {
+            //arrange
+            IStorage? storage = null;
+
+            //act
+
+            //assert
+            Assert.Null(storage);  
+        }
+    }
+}
