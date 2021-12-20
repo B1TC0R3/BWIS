835891feef1d5f1d0adff575b6f3ea8c410cd99c
Thomas Gingele <thomas@gingele.de>
2021-12-20 16:57:55 +0100

    Make Driver contracts public

--- src/BWIS/BWIS.Driver/Contracts/IStorage.cs
+++ src/BWIS/BWIS.Driver/Contracts/IStorage.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BWIS.Driver.Contracts
 {
-   interface IStorage
+    public interface IStorage
     {
     }
 }
--- src/BWIS/BWIS.Driver/Contracts/IStorageItem.cs
+++ src/BWIS/BWIS.Driver/Contracts/IStorageItem.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BWIS.Driver.Contracts
 {
-    internal interface IStorageItem
+    public interface IStorageItem
     {
     }
 }
