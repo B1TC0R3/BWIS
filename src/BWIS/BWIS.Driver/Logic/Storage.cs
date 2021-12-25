37031001f5150ba65241f5a54753ad14d483ab93
Thomas Gingele <thomas@gingele.de>
2021-12-25 14:01:54 +0100

    Add Storage.cs

---
+++ src/BWIS/BWIS.Driver/Logic/Storage.cs
@@ -0,0 +1,42 @@
+﻿using BWIS.Storage.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BWIS.Storage.Logic
+{
+    internal class Storage : IStorage
+    {
+        public void Add(IStorageItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(IStorageItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Replace(int id, IStorageItem newItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Replace(string name, IStorageItem newItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
