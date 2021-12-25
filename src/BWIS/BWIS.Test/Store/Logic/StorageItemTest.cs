0a159036ef0cd2f5d3b3afc500b68254e5a56f52
Thomas Gingele <thomas@gingele.de>
2021-12-25 14:29:01 +0100

    Add Tests for StorageItem

---
+++ src/BWIS/BWIS.Test/Store/Logic/StorageItemTest.cs
@@ -0,0 +1,33 @@
+﻿using BWIS.Store.Contracts;
+using BWIS.Store.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BWIS.Test.Store.Logic
+{
+    public static class StorageItemTest
+    {
+        public static class Add 
+        {
+            [Fact]
+            public static void IsImplementedWithId() 
+            {
+                //arrange
+                IStorageItem item = new StorageItem();
+
+                //act
+                var exception = Record.Exception(() => 
+                {
+                    item.Add(new StorageItem());
+                });
+
+                //assert
+                Assert.Null(exception);
+            }
+        }
+    }
+}
